4a1e63261c266dc93fa9167f690e474b93bcfada
jsekade <jsekade@gmail.com>
2017-03-03 10:01:48 +0100

    Add boostrap/devise

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Pethost
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
   end
+end
+config.generators do |g|
+    	g.orm              :active_record
+    	g.template_engine  :erb
+    	g.stylesheets      false
 end
